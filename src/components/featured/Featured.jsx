01ca01fc34b5f6232062127bd2510245be512998
LONGMANKE <MBURUNJOROGE@GMAIL.com>
2022-07-01 13:26:51 +0100

    rechart

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,7 +4,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 
 
@@ -28,8 +28,22 @@ const Featured = () => {
           <div className="summary">
             <div className="item">
               <div className="itemTitle">Target</div>
-              <div className="itemResult">
+              <div className="itemResult negative">
                 <KeyboardArrowDownIcon fontSize="small"/>
+                <div className="resultAmount">$12.4k</div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="itemTitle">Last Week</div>
+              <div className="itemResult positive ">
+                <KeyboardArrowUpIcon fontSize="small"/>
+                <div className="resultAmount">$12.4k</div>
+              </div>
+            </div>
+            <div className="item">
+              <div className="itemTitle">Last Month</div>
+              <div className="itemResult positive">
+                <KeyboardArrowUpIcon fontSize="small"/>
                 <div className="resultAmount">$12.4k</div>
               </div>
             </div>
