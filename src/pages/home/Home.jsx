ac984b8c46bb1e071d5e57a95f03dd45e63c7495
LONGMANKE <MBURUNJOROGE@GMAIL.com>
2022-07-02 16:29:25 +0100

    btn color

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Featured from "../../components/featured/Featured"
 import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
 import Widget from "../../components/widget/Widget"
+import List from "../../components/table/Table"
 import "./home.scss"
 import React from "react"
 
@@ -23,6 +24,10 @@ const Home = () => {
       <div className="charts">
         <Featured/>
         <Chart/>
+      </div>
+      <div className="listContainer">
+        <div className="listTitle">Latest transaction</div>
+        <List/>
       </div>
       </div>
      </div>
